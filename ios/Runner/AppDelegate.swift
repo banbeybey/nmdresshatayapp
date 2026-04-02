4d6e770512f385adacbbdc0151e67426019db7f5
banbeybey <mertsanverdi98@gmail.com>
2026-04-03 00:59:28 +0300

    Add files via upload

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,33 +1,15 @@
 import Flutter
 import UIKit
-import FirebaseMessaging
+import FirebaseCore
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Storyboard → Flutter geçişinde siyah yerine splash rengi
-    self.window?.backgroundColor = UIColor(
-      red: 240/255, green: 128/255, blue: 144/255, alpha: 1.0
-    )
-
-    UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
-
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-  }
-
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 }
